63ac8a12bd5211638d0bec1dcfcbce54b710361c
BolajiOlajide <bolaji.olajide@andela.com>
2018-01-15 18:32:26 +0100

    refactor code

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+
+# app.config['DEBUG'] = True
+# Uncomment the line above if you want to run the application in dev mode
+# or if you need the server to auto-restart if you make changes
 
 
 @app.route('/')
 def index():
     cryptos = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=10')
-    cryptos = cryptos.json()
-    return render_template('index.html', cryptos=cryptos)
+    return render_template('index.html', cryptos=cryptos.json())
 
 
 if __name__ == '__main__':
