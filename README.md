9d0fdd4e7fc63a6c34bc6fd0c8b282aee5a07cf1
BolajiOlajide <bolaji.olajide@andela.com>
2018-01-15 18:20:02 +0100

    implement html layout and vue instance

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github-Liating
+
+This is a simple Flask-VueJS project that utilizes github's API to show user listing.
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
 @app.route('/')
 def index():
-    return 'Hello it\'s working'
+    cryptos = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+    cryptos = cryptos.json()
+    return render_template('index.html', cryptos=cryptos)
 
 
 if __name__ == '__main__':
